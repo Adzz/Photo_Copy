7dc655a664e5c4fe670a4997d33356e432978da5
Adam Lancaster <lancaster28@hotmail.com>
2016-01-24 19:33:54 +0000

    IMPLEMENTED LIKING FEATURE

--- app/assets/javascripts/likes_before_cool.js
+++ app/assets/javascripts/likes_before_cool.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
  $('.likes-link').on('click', function(event){
+      event.stopImmediatePropagation();
       event.preventDefault();
 
       var likeCount = $(this).siblings('.likes_before_cool_count');
 
       $.post(this.href, function(response){
-        liketCount.text(response.new_likes_before_cool_count);
+        likeCount.text(response.new_likes_before_cool_count);
     })
   })
 })
